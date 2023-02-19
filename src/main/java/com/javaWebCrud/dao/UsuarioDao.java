1dc3d48dec73a4921d18b2d1b4e6f607f7bd25e5
Marcus Oliveira <vinicius.oliveira1935@gmail.com>
2023-02-18 23:45:04 -0300

    Construção da DAO e banco de dados mysql

--- src/main/java/com/javaWebCrud/dao/UsuarioDao.java
+++ src/main/java/com/javaWebCrud/dao/UsuarioDao.java
@@ -2,6 +2,12 @@ package com.javaWebCrud.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.javaWebCrud.bean.Usuario;
 
 public class UsuarioDao {
 
@@ -16,5 +22,46 @@ public class UsuarioDao {
 		}
 		return con;
 	}
+	
+	public static List<Usuario> getAllUsuarios(){
+		List<Usuario> list = new ArrayList<Usuario>();
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM usuario");
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setId(rs.getInt("id"));
+				usuario.setNome(rs.getString("Nome"));
+				usuario.setPassword(rs.getString("password"));
+				usuario.setSexo(rs.getString("sexo"));
+				usuario.setPais(rs.getString("pais"));
+				
+				list.add(usuario);
+			}
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return list;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
